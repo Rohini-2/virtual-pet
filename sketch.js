76dd59ce4466203f915f41154f4cd142634c2270
Rohini-2 <74820598+Rohini-2@users.noreply.github.com>
2021-01-26 17:43:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var dog,sadDog,happyDog;
+
+
+function preload(){
+  sadDog=loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+  createCanvas(1000,600);
+  dog=createSprite(590,500,600,900);
+  dog.addImage(happyDog);
+  dog.scale=0.15;
+  dog=createSprite(600,100,500,200);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+}
+
+function draw() {
+  background(46,139,87);
+  drawSprites();
+}
+
+//function to read food Stock
+
+
+//function to update food stock and last fed time
+
+
+//function to add food in stock
